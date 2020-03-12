fa09b4c9d6408d30b1b1566d59a8d960899e714a
Giacomo1996 <giacomo.ticchi@gmail.com>
2020-03-12 21:08:39 +0100

    C_iterative_algorithm

---
+++ iterative_algorithm.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "math.h"
+
+#define n_iterazioni 6
+
+float num = 1.1640625; //numero di cui voglio effettuare il log2
+
+float a = 0.0;
+
+__uint8_t result[n_iterazioni+1] = {0};
+__int8_t i = 0;
+float risultato = 0.0;
+
+int main(void){
+
+    a = num;
+
+    for(i = 0; i > -n_iterazioni; i--){             
+
+        if(a >= pow(2, pow(2, i))){
+            result[-i] = 1;
+            a = a*pow(2, pow(-2, i));
+        }
+        else
+            result[-i] = 0;
+
+    }
+
+    for(i = 0; i < n_iterazioni; i++){
+        if(result[i] == 1)
+            risultato += pow(2, -i);
+    }
+
+    printf("log2 = %f \r\n", risultato);
+}
