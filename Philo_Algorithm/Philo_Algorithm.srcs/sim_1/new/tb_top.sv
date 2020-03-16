ea6da5d07a05d57daf5ea0c234ca39bfe2bfb231
Andrea Buffoli <andrea.buffoli.6@gmail.com>
2020-03-16 15:28:08 +0100

    version: parametrized extension to improve the precision

--- Philo_Algorithm/Philo_Algorithm.srcs/sim_1/new/tb_top.sv
+++ Philo_Algorithm/Philo_Algorithm.srcs/sim_1/new/tb_top.sv
@@ -5,11 +5,14 @@ module tb_top;
 
     parameter HALF_CLK_PERIOD_NS=20;
     parameter WAIT_CYCLE = 20;
+    
+    parameter               MAN_WIDTH = 16;
+    parameter               OUT_WIDTH = 12;
 
     logic           clk;
     logic           rst;
-    logic [7:0]     initial_value;
-    logic [7:0]     output_value;
+    logic [(MAN_WIDTH-1):0]     initial_value;
+    logic [( OUT_WIDTH-1):0]     output_value;
 
 
     always #HALF_CLK_PERIOD_NS clk = ~clk;
@@ -28,7 +31,7 @@ module tb_top;
     begin
         clk <= 1;
         rst  = 1;
-        initial_value = 8'b10101010;
+        initial_value = 16'b1111_1010_0000_0000;    //1,953125(in base dieci) -> log2(1,953125) = 0.9657842847 (con la calcolatrice)
         repeat(3) @(posedge clk);
         rst  = 0;
         
--- Philo_Algorithm/Philo_Algorithm.srcs/sources_1/new/top.sv
+++ Philo_Algorithm/Philo_Algorithm.srcs/sources_1/new/top.sv
@@ -4,24 +4,27 @@ module top(
     clk,
     rst,
     // inputs
-    initial_value,
+    initial_value,                                              //mantissa -> 1.M     10101010 (che e da vedere come 1.0101010)
     //outputs
-    output_value   
+    output_value                                                //log2(man) -> 0.b7b6b5b4..   
     );
     
-    parameter               comp = 16'b1000_0000_0000_0000;
+    //parameter               comp = 16'b1000_0000_0000_0000;           non serve usare il 2, basta l'MSB
     
+    parameter               MAN_WIDTH = 16;
+    parameter               OUT_WIDTH = 12;                             
+                                                                      
     input                   clk;
     input                   rst;
-    input   logic [7:0]     initial_value;          //fixed point value between 1 and 2, 8 bit
-    output  logic [7:0]     output_value;       
+    input   logic [(MAN_WIDTH-1):0]     initial_value;          //fixed point (GIA ESTESO con 0 come LSBs) value between 1 and 2, 8 bit
+    output  logic [(OUT_WIDTH-1):0]     output_value;       
     
-    logic   [15:0]          PowM, PowM_next;
-    logic   [7:0]           man, man_next;
-    logic   [2:0]           count, count_next;
-    logic   [7:0]           out, out_next;
+    logic   [(2*MAN_WIDTH-1):0]          PowM, PowM_next;       //pow 32 bit (16+16)
+    logic   [(MAN_WIDTH-1):0]           man, man_next;
+    logic   [(OUT_WIDTH-1):0]           out, out_next;
+    logic   [($clog2(OUT_WIDTH)-1):0]           count, count_next;      //a ogni iterazione riempio un bit di out quindi count deve essere parametrizzato a OUT_WIDTH
     
-    typedef enum logic [1:0]
+    typedef enum logic [1:0]                        //stati per la FSM
     {
         IDLE    = 'd0,
         EVAL    = 'd1,
@@ -30,12 +33,12 @@ module top(
     
         ss_mul_div    ss, ss_next;
     
-    always@(posedge clk)
+    always@(posedge clk)                            //SEQUENZIALE
     begin
         if(rst)
         begin
             ss              <=  IDLE;
-            count           <= 'd7;                 //parte da 7 perche la prima comparazione mi da il msb della parte frazionaria 0.b7b6b5b4...
+            count           <= (OUT_WIDTH-1);                 //parte da (OUT_WIDTH-1) perche la prima comparazione mi da il msb della parte frazionaria 0.b7b6b5b4...
             man             <= '0;
             PowM            <= '0;
             out             <= '0;
@@ -51,7 +54,7 @@ module top(
     end
     
     
-    always_comb
+    always_comb                                 //COMBINATORIA
     begin
         ss_next     =   ss;
         count_next  =   count;
@@ -63,24 +66,24 @@ module top(
             IDLE:
             begin
                 ss_next         = EVAL;
-                count_next      = 'd7;          
+                count_next      = (OUT_WIDTH-1);          
                 man_next        = initial_value;
                 PowM_next       = initial_value*initial_value;
             end                                                             //end IDLE
             EVAL:
             begin
 
-                if(PowM[15])
+                if(PowM[(2*MAN_WIDTH-1)])
                 begin
-                    out_next[count] = 1;
-                    man_next    =   PowM[15:8];
-                    PowM_next   =   PowM[15:8]*PowM[15:8]; 
+                    out_next[count] = 1;                            //pow <=2 quindi il bit e' 1
+                    man_next    =   PowM[(2*MAN_WIDTH-1):16];       //il prossimo operand e' la potenza diviso due (quindi prendo i 16 MSB di pow)     
+                    PowM_next   =   PowM[(2*MAN_WIDTH-1):16]*PowM[(2*MAN_WIDTH-1):16];          //la prossima potenza e' man_next*man_next 
                 end
                 else
                 begin
                     out_next[count] = 0;
-                    man_next    =   PowM[14:7];
-                    PowM_next   =   PowM[14:7]*PowM[14:7];
+                    man_next    =   PowM[(2*MAN_WIDTH-2):15];                           //il prossimo operand e' la potenza stessa (quindi prendo i 16 bit dopo l'MSB che sara' 0 essendo <2)
+                    PowM_next   =   PowM[(2*MAN_WIDTH-2):15]*PowM[(2*MAN_WIDTH-2):15];  //la prossima potenza e' man_next * man_next
                 end
                 
                 if(count == 0)
