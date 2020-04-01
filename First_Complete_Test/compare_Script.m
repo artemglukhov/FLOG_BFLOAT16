edd1b2c884d3b20660c1c778ee1e6f54cf8e3ede
artemglukhov <glukosio@ymail.com>
2020-04-01 21:52:25 +0200

    fixed and optimized the script

--- First_Complete_Test/compare_Script.m
+++ First_Complete_Test/compare_Script.m
@@ -1,17 +1,29 @@
 %input:       0, 11110111, 0101011
 %output_RTL:  0, 10000101, 1110000
+close all;
 
-C=readtable('/Users/glukosio/Documents/FLOG_BFLOAT16/First_Complete_Test/result.csv', 'format', '%s %s %s');
+C=readtable('/Users/glukosio/Documents/FLOG_BFLOAT16/First_Complete_Test/results2.txt', 'format', '%s %s %s');
 matrix=table2array(C);
 [x,y]=size(matrix);
-log_input=zeros(1000,1);
-output_RTL=zeros(1000,1);
-
-for i = 1 : x
-    if mod(i,2) == 1
-        input1= (-1)^(bin2dec(matrix(i,1)))*2^(bin2dec(matrix(i,2))-127)*(1+bin2dec(matrix(i,3))*2^(-7));
-        log_input(i) = log2(input1);
-    else
-        output_RTL(i-1)=(-1)^(bin2dec(matrix(i,1)))*2^(bin2dec(matrix(i,2))-127)*(1+bin2dec(matrix(i,3))*2^(-7));
+log_input=zeros(x,1);
+output_RTL=zeros(x,1);
+diff=zeros(x/2,2);
+j=1;
+for i = 1 :2: x
+    input1= (-1)^(bin2dec(matrix(i,1)))*2^(bin2dec(matrix(i,2))-127)*(1+bin2dec(matrix(i,3))*2^(-7));
+    log_input(i) = log2(input1);
+    output_RTL(i)=(-1)^(bin2dec(matrix(i+1,1)))*2^(bin2dec(matrix(i+1,2))-127)*(1+bin2dec(matrix(i+1,3))*2^(-7));
+    
+    if abs(log_input(i) - output_RTL(i))<1
+        diff(j,1)=log_input(i);
+        diff(j,2)=log_input(i) - output_RTL(i);
+        j=j+1;
     end
 end
+
+
+%------- PLOTS -------%
+histogram(diff(:,2), 100);
+figure;
+stem(diff(:,1),diff(:,2));
+grid on;
